68fc6009ce3d4b4c9c53ad60ab857def02ee35db
Rizqan Akbar Mutaram <rizqanakbarmutaram1@gmail.com>
2026-02-26 13:24:55 +0700

    Delete Pertemuan17 directory

--- Pertemuan17/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$koneksi = mysqli_connect("localhost","root","","pendaftaran_mahasiswa");
-
-if(!$koneksi) {
-  die("Koneksi gagal:" . mysqli_connect_error());
-}
-?>
--- Pertemuan17/hapus.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'config.php';
-
-$id = $_GET['id'];
-mysqli_query($koneksi,"DELETE FROM calon_mahasiswa WHERE id=$id");
-
-header("Location: list-mahasiswa.php");
-?>
